f7937d11871b6b9af2280f264f3cea0e9cfac56c
TheSnak7 <51061669+TheSnak7@users.noreply.github.com>
2024-09-16 03:18:24 +0200

    Fix idiomatic spelling of const trait parameter

--- src/stack.rs
+++ src/stack.rs
@@ -4,12 +4,12 @@ use crate::lox_value::LoxValue;
 
 #[derive(Debug)]
 #[repr(transparent)]
-pub struct Sp<const StackSize: usize> {
+pub struct Sp<const STACK_SIZE: usize> {
     ptr: NonNull<LoxValue>,
 }
 
-impl<const StackSize: usize> Sp<StackSize> {
-    pub unsafe fn create(stack: &mut Stack<StackSize>) -> Sp<StackSize> {
+impl<const STACK_SIZE: usize> Sp<STACK_SIZE> {
+    pub unsafe fn create(stack: &mut Stack<STACK_SIZE>) -> Sp<STACK_SIZE> {
         let ptr = stack.bytes.as_mut_ptr();
         assert!(ptr != std::ptr::null_mut());
         Sp {
@@ -18,7 +18,7 @@ impl<const StackSize: usize> Sp<StackSize> {
     }
 }
 
-impl<const StackSize: usize> Sp<StackSize> {
+impl<const STACK_SIZE: usize> Sp<STACK_SIZE> {
     #[inline(always)]
     pub fn get_value(&self) -> LoxValue {
         unsafe { *self.ptr.as_ref() }
@@ -40,54 +40,54 @@ impl<const StackSize: usize> Sp<StackSize> {
     }
 }
 
-impl<const StackSize: usize> PartialEq for Sp<StackSize> {
+impl<const STACK_SIZE: usize> PartialEq for Sp<STACK_SIZE> {
     fn eq(&self, other: &Self) -> bool {
         return self.ptr == other.ptr;
     }
 }
 
-impl<const StackSize: usize> PartialOrd for Sp<StackSize> {
+impl<const STACK_SIZE: usize> PartialOrd for Sp<STACK_SIZE> {
     fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
         self.ptr.partial_cmp(&other.ptr)
     }
 }
 
-impl<const StackSize: usize> Clone for Sp<StackSize> {
+impl<const STACK_SIZE: usize> Clone for Sp<STACK_SIZE> {
     fn clone(&self) -> Self {
         Self { ptr: self.ptr }
     }
 }
 
 #[derive(Debug)]
-pub struct Stack<const StackSize: usize> {
-    bytes: Pin<Box<[LoxValue; StackSize]>>,
+pub struct Stack<const STACK_SIZE: usize> {
+    bytes: Pin<Box<[LoxValue; STACK_SIZE]>>,
 }
 
-impl<const StackSize: usize> Stack<StackSize> {
-    pub fn new() -> Stack<StackSize> {
-        let heap_vec = vec![LoxValue::default(); StackSize];
-        let heap_array: Box<[LoxValue; StackSize]> = heap_vec.try_into().unwrap();
+impl<const STACK_SIZE: usize> Stack<STACK_SIZE> {
+    pub fn new() -> Stack<STACK_SIZE> {
+        let heap_vec = vec![LoxValue::default(); STACK_SIZE];
+        let heap_array: Box<[LoxValue; STACK_SIZE]> = heap_vec.try_into().unwrap();
         Self {
             bytes: Box::into_pin(heap_array),
         }
     }
 
-    pub fn get_base_sp(&mut self) -> Sp<StackSize> {
-        unsafe { Sp::<StackSize>::create(self) }
+    pub fn get_base_sp(&mut self) -> Sp<STACK_SIZE> {
+        unsafe { Sp::<STACK_SIZE>::create(self) }
     }
 
-    pub fn get_stack_iterator(&mut self, up_to: Sp<StackSize>) -> StackIterator<StackSize> {
+    pub fn get_stack_iterator(&mut self, up_to: Sp<STACK_SIZE>) -> StackIterator<STACK_SIZE> {
         StackIterator::new(self, &up_to)
     }
 }
 
-pub struct StackIterator<const StackSize: usize> {
-    curr: Sp<StackSize>,
-    top: Sp<StackSize>,
+pub struct StackIterator<const STACK_SIZE: usize> {
+    curr: Sp<STACK_SIZE>,
+    top: Sp<STACK_SIZE>,
 }
 
-impl<const StackSize: usize> StackIterator<StackSize> {
-    pub fn new(stack: &mut Stack<StackSize>, sp: &Sp<StackSize>) -> Self {
+impl<const STACK_SIZE: usize> StackIterator<STACK_SIZE> {
+    pub fn new(stack: &mut Stack<STACK_SIZE>, sp: &Sp<STACK_SIZE>) -> Self {
         let base = stack.get_base_sp();
         let top = sp.clone();
 
@@ -98,7 +98,7 @@ impl<const StackSize: usize> StackIterator<StackSize> {
     }
 }
 
-impl<const StackSize: usize> Iterator for StackIterator<StackSize> {
+impl<const STACK_SIZE: usize> Iterator for StackIterator<STACK_SIZE> {
     type Item = LoxValue;
 
     fn next(&mut self) -> Option<Self::Item> {
